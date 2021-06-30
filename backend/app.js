eb95e414704878ce644fd8c2f436aff7b420e2e4
bertech <santosc.aj@gmail.com>
2021-06-29 20:19:47 -0400

    updating CORS

--- backend/app.js
+++ backend/app.js
@@ -58,7 +58,27 @@ function dataCleaner(req, res, next){
     next()
 }
 
-app.use(cors({origin:'http://localhost:8080'}))
+// app.use(cors({origin:'http://localhost:8080'}))
+
+
+let allowedOrigins = [
+    'http://localhost','http://localhost:8080','http://localhost:80', 
+    'https://todolist.santosaj.com', 'http://todolist.santosaj.com'
+	]
+
+app.use(cors({origin: function(origin, callback){
+    // allow requests with no origin 
+    // (like mobile apps or curl requests)
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+
+}))
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
